f3bf360ac19428cd4132911e6cde4dfb896f6625
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-07 01:41:40 +0100

    create auth service #2

---
+++ src/app/core/services/authentication.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  constructor() {}
+
+  signin(value: any) {
+    return value;
+  }
+}
