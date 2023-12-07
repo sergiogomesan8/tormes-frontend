6f7651667ec1dcfb0a4e35ccee8a59b7fe69b24d
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-07 20:22:16 +0100

    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,16 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SharedModule } from './shared/shared.module';
-
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    SharedModule,
-
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    
+
     MatFormFieldModule,
   ],
   providers: [provideClientHydration()],
