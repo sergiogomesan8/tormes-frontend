d77dcc1188a6e1d9c2e9d43d3a763cab5fe5a9ee
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-07 01:42:06 +0100

    update shared module #2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterComponent } from './components/register/register.component';
 
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationService } from '../core/services/authentication.service';
 
 @NgModule({
-  imports: [CommonModule, MatFormFieldModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
   declarations: [RegisterComponent],
+  providers: [AuthenticationService],
 })
 export class SharedModule {}
