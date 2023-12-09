6681be3d97b3f108c959a8059c2d3baf345468b5
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-09 20:21:13 +0100

    new search component #12

--- projects/tormes-library/src/lib/components/components.module.ts
+++ projects/tormes-library/src/lib/components/components.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { MatIconModule } from '@angular/material/icon';
+import { SearchComponent } from './search/search.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [ButtonComponent],
-  exports: [ButtonComponent],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  declarations: [ButtonComponent, SearchComponent],
+  exports: [ButtonComponent, SearchComponent],
 })
 export class ComponentsModule {}
