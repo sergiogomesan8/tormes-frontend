3b1a6cebeb53aac6f2065e6be4bae9c84980456d
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-10 21:29:05 +0100

    update product component test #10

--- src/app/shop/customer/components/product/product.component.spec.ts
+++ src/app/shop/customer/components/product/product.component.spec.ts
@@ -4,21 +4,30 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { ProductComponent } from './product.component';
+import { Product } from '@shop/models/product';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
+  let product: Product;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+    let product: Product = {
+      id: '1',
+      image: 'root/to/product.jpg',
+      name: 'product name',
+      price: 100,
+      section: 'product section'
+    };
+    component.product = product;
     fixture.detectChanges();
   });
 
