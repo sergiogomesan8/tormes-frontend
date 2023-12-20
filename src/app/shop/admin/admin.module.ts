16770c72cd9b3fb10fafc381b5da2d197a64c74e
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-20 01:09:57 +0100

    update admin module #18

--- src/app/shop/admin/admin.module.ts
+++ src/app/shop/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
+import { SharedModule } from '@shared/shared.module';
+import { AdminComponent } from './admin.component';
 
 @NgModule({
-  imports: [CommonModule, AdminRoutingModule],
-  declarations: [],
+  imports: [CommonModule, AdminRoutingModule, SharedModule],
+  declarations: [AdminComponent],
 })
 export class AdminModule {}
