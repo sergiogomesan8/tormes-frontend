b8e42f71b303ede74f486b48194bc9b9a2b669ab
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-22 13:51:36 +0100

    product service update delete method added #23

--- src/app/shop/services/product.service.ts
+++ src/app/shop/services/product.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpService } from '@core/services/http-service.service';
+import { TranslateService } from '@ngx-translate/core';
 import { ProductEndPoint } from '@shared/end-points';
+import { SnackbarService } from '@shared/services/snackbar.service';
 import { Product } from '@shop/models/product';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +13,10 @@ import { Observable, catchError, of } from 'rxjs';
 export class ProductService {
   private productEndPoint = new ProductEndPoint();
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly snackbarService: SnackbarService
+  ) {}
 
   findAllProducts(): Observable<Product[]> {
     return this.httpService.get(this.productEndPoint.FIND_ALL).pipe(
@@ -18,5 +24,43 @@ export class ProductService {
         return of([]);
       })
     );
+  }
+
+  delete(product: Product): Observable<undefined> {
+    return this.httpService
+      .delete(`${this.productEndPoint.DELETE}${product.id}`, product)
+      .pipe(
+        map((response: undefined) => {
+          this.snackbarService.showSuccessSnackbar(
+            'shop.admin.dashboard.options.products.deleteSuccess'
+          );
+          return response;
+        }),
+        catchError((error: undefined) => {
+          this.snackbarService.showErrorSnackbar(
+            'shop.admin.dashboard.options.products.deleteError'
+          );
+          return of(error);
+        })
+      );
+  }
+
+  update(product: Product): Observable<Product | undefined> {
+    return this.httpService
+      .patch(`${this.productEndPoint.UPDATE}${product.id}`, product)
+      .pipe(
+        map((response: undefined) => {
+          this.snackbarService.showSuccessSnackbar(
+            'shop.admin.dashboard.options.products.updateSuccess'
+          );
+          return response;
+        }),
+        catchError((error: undefined) => {
+          this.snackbarService.showErrorSnackbar(
+            'shop.admin.dashboard.options.products.updateError'
+          );
+          return of(error);
+        })
+      );
   }
 }
