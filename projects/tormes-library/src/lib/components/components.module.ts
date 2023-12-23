b77c4c1f8f5a133ca4ce194461ae962d86627dfc
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-23 01:20:52 +0100

    add file upload component to components module #23

--- projects/tormes-library/src/lib/components/components.module.ts
+++ projects/tormes-library/src/lib/components/components.module.ts
@@ -4,10 +4,11 @@ import { ButtonComponent } from './button/button.component';
 import { MatIconModule } from '@angular/material/icon';
 import { SearchComponent } from './search/search.component';
 import { MatButtonModule } from '@angular/material/button';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   imports: [CommonModule, MatButtonModule, MatIconModule],
-  declarations: [ButtonComponent, SearchComponent],
-  exports: [ButtonComponent, SearchComponent],
+  declarations: [ButtonComponent, SearchComponent, FileUploadComponent],
+  exports: [ButtonComponent, SearchComponent, FileUploadComponent],
 })
 export class ComponentsModule {}
