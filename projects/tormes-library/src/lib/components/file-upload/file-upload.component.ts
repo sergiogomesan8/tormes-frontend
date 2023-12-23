daf22b128553eaff2d6ab6971910cdbe225da35c
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-23 14:34:31 +0100

    updatefile upload component #23

--- projects/tormes-library/src/lib/components/file-upload/file-upload.component.ts
+++ projects/tormes-library/src/lib/components/file-upload/file-upload.component.ts
@@ -3,7 +3,6 @@ import {
   Component,
   EventEmitter,
   Input,
-  OnInit,
   Output,
 } from '@angular/core';
 
@@ -12,17 +11,13 @@ import {
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.scss'],
 })
-export class FileUploadComponent implements OnInit {
+export class FileUploadComponent {
   @Input() label: string = '';
   @Output() fileSelected: EventEmitter<File> = new EventEmitter<File>();
 
   previewUrl: string | ArrayBuffer = 'assets/images/icons/upload-file.png';
 
   constructor(private cdr: ChangeDetectorRef) {}
-
-  ngOnInit() {
-    console.log(this.previewUrl);
-  }
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
