33997c5187984dbe31e8c49c8aa482f5f61b873b
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-24 13:56:32 +0100

    add create product componet tests #23

--- src/app/shop/admin/components/create-product/create-product.component.spec.ts
+++ src/app/shop/admin/components/create-product/create-product.component.spec.ts
@@ -1,28 +1,108 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductService } from '@shop/services/product.service';
 import { CreateProductComponent } from './create-product.component';
+import { of, throwError } from 'rxjs';
 
 describe('CreateProductComponent', () => {
   let component: CreateProductComponent;
-  let fixture: ComponentFixture<CreateProductComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CreateProductComponent ]
-    })
-    .compileComponents();
-  }));
+  let mockProductService: jest.Mocked<ProductService>;
+  let mockRouter: jest.Mocked<Router>;
+  let formBuilder: FormBuilder;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CreateProductComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockProductService = {
+      addProduct: jest.fn(),
+    } as any;
+
+    mockRouter = {
+      navigate: jest.fn(),
+    } as any;
+
+    formBuilder = new FormBuilder();
+
+    component = new CreateProductComponent(
+      mockProductService,
+      formBuilder,
+      mockRouter
+    );
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('addProduct', () => {
+    it('should add product and navigate on success', () => {
+      component.formGroup = formBuilder.group({
+        name: ['test', [Validators.required]],
+        section: ['test', [Validators.required]],
+        price: [
+          '10',
+          [Validators.required, Validators.pattern('^[0-9]+(.[0-9]{1,2})?$')],
+        ],
+        description: ['test', [Validators.required]],
+        image: ['test', [Validators.required]],
+      });
+
+      const productDto = {
+        name: 'test',
+        section: 'test',
+        price: 10,
+        description: 'test',
+        image: 'test',
+      };
+
+      mockProductService.addProduct.mockReturnValue(of(undefined));
+
+      component.addProduct();
+
+      expect(mockProductService.addProduct).toHaveBeenCalledWith(productDto);
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/admin/products']);
+    });
+
+    it('should log error on failure', () => {
+      const consoleSpy = jest.spyOn(console, 'log');
+      const error = new Error('Test error');
+
+      component.formGroup = formBuilder.group({
+        name: ['test', [Validators.required]],
+        section: ['test', [Validators.required]],
+        price: [
+          '10',
+          [Validators.required, Validators.pattern('^[0-9]+(.[0-9]{1,2})?$')],
+        ],
+        description: ['test', [Validators.required]],
+        image: ['test', [Validators.required]],
+      });
+
+      mockProductService.addProduct.mockReturnValue(throwError(() => error));
+
+      component.addProduct();
+
+      expect(consoleSpy).toHaveBeenCalledWith(error);
+    });
+
+    it('should not add product if form is invalid', () => {
+      component.formGroup = formBuilder.group({
+        name: ['', [Validators.required]],
+        section: ['test', [Validators.required]],
+        price: [
+          '10',
+          [Validators.required, Validators.pattern('^[0-9]+(.[0-9]{1,2})?$')],
+        ],
+        description: ['test', [Validators.required]],
+        image: ['test', [Validators.required]],
+      });
+
+      component.addProduct();
+
+      expect(mockProductService.addProduct).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('handleFile', () => {
+    it('should set image path on file handle', () => {
+      const file = new File([''], 'test.png', { type: 'image/png' });
+      component.handleFile(file);
+      expect(component.formGroup.get('image')?.value).toBe('/image/test.png');
+    });
   });
 });
