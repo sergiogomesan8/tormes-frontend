cbc213dccf7ef869b55ddda958a2953af31a00e1
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-24 13:45:20 +0100

    add snackbar service tests #23

--- src/app/shared/services/snackbar.service.spec.ts
+++ src/app/shared/services/snackbar.service.spec.ts
@@ -1,16 +1,55 @@
 /* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { SnackbarService } from './snackbar.service';
+import { TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
 
 describe('Service: Snackbar', () => {
+  let service: SnackbarService;
+  let snackBar: jest.Mocked<MatSnackBar>;
+  let translateService: jest.Mocked<TranslateService>;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [SnackbarService]
+    snackBar = { open: jest.fn() } as any;
+    translateService = { get: jest.fn() } as any;
+
+    service = new SnackbarService(snackBar, translateService);
+  });
+
+  describe('showSuccessSnackbar', () => {
+    it('should call snackBar.open', () => {
+      translateService.get.mockReturnValue(of('message'));
+
+      service.showSuccessSnackbar('messageKey');
+
+      expect(snackBar.open).toHaveBeenCalledWith('message', 'Success', {
+        duration: 2000,
+        verticalPosition: 'top',
+        panelClass: ['green-snackbar'],
+      });
+    });
+
+    it('should call translateService.get with correct message key', () => {
+      service.showSuccessSnackbar('messageKey');
+      expect(translateService.get).toHaveBeenCalledWith('messageKey');
     });
   });
 
-  it('should ...', inject([SnackbarService], (service: SnackbarService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('showErrorSnackbar', () => {
+    it('should call snackBar.open', () => {
+      translateService.get.mockReturnValue(of('message'));
+
+      service.showErrorSnackbar('messageKey');
+
+      expect(snackBar.open).toHaveBeenCalledWith('message', 'Error', {
+        duration: 2000,
+        verticalPosition: 'top',
+        panelClass: ['red-snackbar'],
+      });
+    });
+    it('should call translateService.get with correct message key', () => {
+      service.showErrorSnackbar('messageKey');
+      expect(translateService.get).toHaveBeenCalledWith('messageKey');
+    });
+  });
 });
