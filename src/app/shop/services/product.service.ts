6ed3f58eac6063a1b8159c5c6bff580a62bbf70f
SergioGómez <sergio.gomesan8@gmail.com>
2023-12-24 13:17:03 +0100

    add fint by id method to product service #23

--- src/app/shop/services/product.service.ts
+++ src/app/shop/services/product.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpService } from '@core/services/http-service.service';
-import { TranslateService } from '@ngx-translate/core';
 import { ProductEndPoint } from '@shared/end-points';
 import { SnackbarService } from '@shared/services/snackbar.service';
+import {
+  CreateProductDto,
+  UpdateProductDto,
+} from '@shop/admin/dtos/product.dto';
 import { Product } from '@shop/models/product';
 import { Observable, catchError, map, of } from 'rxjs';
 
@@ -18,6 +20,22 @@ export class ProductService {
     private readonly snackbarService: SnackbarService
   ) {}
 
+  findProductById(productId: string): Observable<Product | undefined> {
+    return this.httpService
+      .get(`${this.productEndPoint.FIND_BY_ID}${productId}`)
+      .pipe(
+        map((response: undefined) => {
+          return response;
+        }),
+        catchError((error: undefined) => {
+          this.snackbarService.showErrorSnackbar(
+            'shop.admin.dashboard.options.products.updateError'
+          );
+          return of(error);
+        })
+      );
+  }
+
   findAllProducts(): Observable<Product[]> {
     return this.httpService.get(this.productEndPoint.FIND_ALL).pipe(
       catchError((error: undefined) => {
@@ -26,7 +44,29 @@ export class ProductService {
     );
   }
 
-  delete(product: Product): Observable<undefined> {
+  addProduct(
+    createProductDto: CreateProductDto
+  ): Observable<Product | undefined> {
+    console.log(createProductDto);
+    return this.httpService
+      .post(this.productEndPoint.DELETE, createProductDto)
+      .pipe(
+        map((response: Product) => {
+          this.snackbarService.showSuccessSnackbar(
+            'shop.admin.dashboard.options.products.addSuccess'
+          );
+          return response;
+        }),
+        catchError((error: undefined) => {
+          this.snackbarService.showErrorSnackbar(
+            'shop.admin.dashboard.options.products.addError'
+          );
+          return of(error);
+        })
+      );
+  }
+
+  deleteProduct(product: Product): Observable<undefined> {
     return this.httpService
       .delete(`${this.productEndPoint.DELETE}${product.id}`, product)
       .pipe(
@@ -45,9 +85,9 @@ export class ProductService {
       );
   }
 
-  update(product: Product): Observable<Product | undefined> {
+  updateProduct(productId:string, product: UpdateProductDto): Observable<Product | undefined> {
     return this.httpService
-      .patch(`${this.productEndPoint.UPDATE}${product.id}`, product)
+      .patch(`${this.productEndPoint.UPDATE}${productId}`, product)
       .pipe(
         map((response: undefined) => {
           this.snackbarService.showSuccessSnackbar(
