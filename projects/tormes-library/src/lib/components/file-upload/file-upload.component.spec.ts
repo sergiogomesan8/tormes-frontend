d615f1221ddbb27fd62b4cf778f92c0b20c131f5
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-02 20:18:21 +0100

    upate file upload component tests #27

--- projects/tormes-library/src/lib/components/file-upload/file-upload.component.spec.ts
+++ projects/tormes-library/src/lib/components/file-upload/file-upload.component.spec.ts
@@ -15,14 +15,15 @@ describe('FileUploadComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should emit fileSelected event when onFileSelected is called with a file', () => {
-    const mockFile = new File([''], 'filename', { type: 'text/html' });
+  it('should emit fileSelected event with file when onFileSelected is called with a valid file', () => {
+    const mockFile = new File([''], 'filename', { type: 'image/png' });
     const mockEvt = { target: { files: [mockFile] } };
 
     const reader = {
       readAsDataURL: jest.fn(),
       result: 'result',
       onload: jest.fn(),
+      onerror: jest.fn(),
     };
 
     window.FileReader = jest.fn(() => reader) as any;
@@ -33,7 +34,30 @@ describe('FileUploadComponent', () => {
       reader.onload();
     }
 
-    expect(component.fileSelected.emit).toHaveBeenCalledWith(mockFile);
+    expect(component.fileSelected.emit).toHaveBeenCalledWith({
+      file: mockFile,
+    });
     expect(component.previewUrl).toEqual(reader.result);
+  });
+
+  it('should emit fileSelected event with error when onFileSelected is called with an invalid file', () => {
+    const mockFile = new File([''], 'filename', { type: 'invalid/type' });
+    const mockEvt = { target: { files: [mockFile] } };
+
+    component.onFileSelected(mockEvt);
+
+    expect(component.fileSelected.emit).toHaveBeenCalledWith({
+      error: 'Invalid file type',
+    });
+  });
+
+  it('should emit fileSelected event with error when onFileSelected is called with no file', () => {
+    const mockEvt = { target: { files: [] } };
+
+    component.onFileSelected(mockEvt);
+
+    expect(component.fileSelected.emit).toHaveBeenCalledWith({
+      error: 'No file selected',
+    });
   });
 });
