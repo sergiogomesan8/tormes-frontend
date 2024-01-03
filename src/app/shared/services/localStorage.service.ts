f96973c318cfd9aafdf3c5e2e35fb030195d1b93
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-03 17:06:52 +0100

    local storage not defined (angular universal error) solved #30

---
+++ src/app/shared/services/localStorage.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+
+class LocalStorage implements Storage {
+  [name: string]: any;
+  readonly length: number;
+
+  constructor() {
+    this.length = 0;
+  }
+
+  clear(): void {}
+
+  getItem(key: string): string | null {
+    return null;
+  }
+
+  key(index: number): string | null {
+    return null;
+  }
+
+  removeItem(key: string): void {}
+
+  setItem(key: string, value: string): void {}
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalstorageService implements Storage {
+  private storage: Storage;
+  length: number;
+
+  constructor() {
+    this.storage = new LocalStorage();
+    this.length = 0;
+
+    AppComponent.isBrowser.subscribe((isBrowser) => {
+      if (isBrowser) {
+        this.storage = localStorage;
+      }
+    });
+  }
+
+  [name: string]: any;
+
+  clear(): void {
+    this.storage.clear();
+  }
+
+  getItem(key: string): string | null {
+    return this.storage.getItem(key);
+  }
+
+  key(index: number): string | null {
+    return this.storage.key(index);
+  }
+
+  removeItem(key: string): void {
+    return this.storage.removeItem(key);
+  }
+
+  setItem(key: string, value: string): void {
+    return this.storage.setItem(key, value);
+  }
+}
