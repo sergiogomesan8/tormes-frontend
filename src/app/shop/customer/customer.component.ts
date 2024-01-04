a0b36b4b9ac4a1b4f60f51f52970c13b9124c662
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-05 00:48:22 +0100

    add cutomer navbar to customer component #30

--- src/app/shop/customer/customer.component.ts
+++ src/app/shop/customer/customer.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '@core/services/authentication.service';
+import { User } from '@shop/models/user.model';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
 })
-export class CustomerComponent {}
+export class CustomerComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  userInfo: User | undefined;
+
+  constructor(private authenticationService: AuthenticationService) {}
+
+  ngOnInit(): void {
+    this.userInfo = this.authenticationService.getUserInfo();
+    this.isLoggedIn = !!this.userInfo;
+  }
+}
