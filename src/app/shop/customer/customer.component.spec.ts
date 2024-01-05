ce9bb16118fd227a7448b6f56c84c7a0a9b40b08
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-05 01:07:25 +0100

    add customer component tests #30

--- src/app/shop/customer/customer.component.spec.ts
+++ src/app/shop/customer/customer.component.spec.ts
@@ -1,20 +1,26 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomerComponent } from './customer.component';
+import { AuthenticationService } from '@core/services/authentication.service';
+import { User } from '@shop/models/user.model';
 
 describe('CustomerComponent', () => {
   let component: CustomerComponent;
   let fixture: ComponentFixture<CustomerComponent>;
+  let authServiceMock: jest.Mocked<AuthenticationService>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CustomerComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    authServiceMock = {
+      getUserInfo: jest.fn(),
+    } as unknown as jest.Mocked<AuthenticationService>;
+
+    await TestBed.configureTestingModule({
+      declarations: [CustomerComponent],
+      providers: [
+        { provide: AuthenticationService, useValue: authServiceMock },
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CustomerComponent);
@@ -24,5 +30,19 @@ describe('CustomerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set userInfo and isLoggedIn correctly on ngOnInit', () => {
+    const user: User = {
+      id: 'id',
+      name: 'Test User',
+      email: 'test@mail.com',
+    };
+    authServiceMock.getUserInfo.mockReturnValue(user);
+
+    component.ngOnInit();
+
+    expect(component.userInfo).toEqual(user);
+    expect(component.isLoggedIn).toBe(true);
   });
 });
