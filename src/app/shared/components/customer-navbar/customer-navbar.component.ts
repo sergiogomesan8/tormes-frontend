6bc575407d7feb296f1da1707a943fdccd868ea4
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-07 00:13:24 +0100

    add logout call to component #31

--- src/app/shared/components/customer-navbar/customer-navbar.component.ts
+++ src/app/shared/components/customer-navbar/customer-navbar.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { User } from '@shop/models/user.model';
 
@@ -10,8 +16,13 @@ import { User } from '@shop/models/user.model';
 export class CustomerNavbarComponent {
   @Input() customer: User | undefined;
   @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
+  @Output() logoutButtonClick: EventEmitter<void> = new EventEmitter<void>();
 
   someMethod() {
     this.trigger.openMenu();
+  }
+
+  logout() {
+    this.logoutButtonClick.emit();
   }
 }
--- src/app/shop/customer/customer.component.ts
+++ src/app/shop/customer/customer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AuthenticationService } from '@core/services/authentication.service';
 import { User } from '@shop/models/user.model';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-customer',
@@ -10,10 +12,20 @@ export class CustomerComponent implements OnInit {
   isLoggedIn: boolean = false;
   userInfo: User | undefined;
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private readonly router: Router
+  ) {}
 
   ngOnInit(): void {
     this.userInfo = this.authenticationService.getUserInfo();
     this.isLoggedIn = !!this.userInfo;
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/catalog']).then(() => {
+      window.location.reload();
+    });
   }
 }
