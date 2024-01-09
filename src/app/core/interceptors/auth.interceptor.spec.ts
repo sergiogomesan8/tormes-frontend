e6189bdfddb842339d948e6e6423f6a49c03726e
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-10 00:21:46 +0100

    update auth interceptor tests #31

--- src/app/core/interceptors/auth.interceptor.spec.ts
+++ src/app/core/interceptors/auth.interceptor.spec.ts
@@ -1,47 +1,99 @@
-import { HttpRequest } from '@angular/common/http';
-import { AuthInterceptor } from './auth.interceptor';
 import { of } from 'rxjs';
+import { AuthInterceptor } from './auth.interceptor';
+import { HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
+import * as jwtDecode from 'jwt-decode';
+import { AuthenticationService } from '@core/services/authentication.service';
 
 describe('AuthInterceptor', () => {
+  const mockJwtDecode = jest.fn();
+
   let interceptor: AuthInterceptor;
-  let authServiceMock: any;
-  let httpHandlerMock: any;
+  let authServiceMock: jest.Mocked<AuthenticationService>;
+  let httpHandlerMock: jest.Mocked<HttpHandler>;
+  let httpRequestMock: jest.Mocked<HttpRequest<any>>;
 
   beforeEach(() => {
-    authServiceMock = { getToken: jest.fn() };
-    httpHandlerMock = { handle: jest.fn() };
+    authServiceMock = {
+      getToken: jest.fn(),
+      isRefreshingToken: false,
+      refreshToken: jest.fn(),
+    } as any;
+
+    httpRequestMock = {
+      clone: jest.fn(),
+    } as any;
+
+    httpHandlerMock = {
+      handle: jest.fn(),
+    } as any;
+
+    jest.spyOn(jwtDecode, 'jwtDecode').mockImplementation(() => ({
+      exp: Date.now() / 1000 + 60,
+    }));
+
     interceptor = new AuthInterceptor(authServiceMock);
   });
 
-  it('should add Authorization header when token is available', () => {
-    const request = new HttpRequest('GET', 'http://localhost');
-    const token = 'test-token';
+  it('should handle request without token', () => {
+    authServiceMock.getToken.mockReturnValue('');
+
+    interceptor.intercept(httpRequestMock, httpHandlerMock);
+
+    expect(httpHandlerMock.handle).toHaveBeenCalledWith(httpRequestMock);
+  });
+
+  it('should add token to request if token is not expired', () => {
+    const token = 'token';
     authServiceMock.getToken.mockReturnValue(token);
-    httpHandlerMock.handle.mockReturnValue(of(null));
 
-    interceptor.intercept(request, httpHandlerMock);
+    mockJwtDecode.mockImplementation(() => ({
+      exp: Date.now() / 1000 + 60,
+    }));
 
-    expect(httpHandlerMock.handle).toHaveBeenCalledWith(
-      expect.objectContaining({
-        headers: expect.objectContaining({
-          get: expect.any(Function),
-          has: expect.any(Function),
-        }),
-      })
-    );
+    interceptor.intercept(httpRequestMock, httpHandlerMock);
 
-    expect(
-      httpHandlerMock.handle.mock.calls[0][0].headers.get('Authorization')
-    ).toBe(`Bearer ${token}`);
+    expect(httpRequestMock.clone).toHaveBeenCalledWith({
+      setHeaders: { Authorization: `Bearer ${token}` },
+    });
   });
 
-  it('should not add Authorization header when token is not available', () => {
-    const request = new HttpRequest('GET', 'http://localhost');
-    authServiceMock.getToken.mockReturnValue(null);
-    httpHandlerMock.handle.mockReturnValue(of(null));
+  it('should handle request when isRefreshingToken is true', () => {
+    authServiceMock.isRefreshingToken = true;
 
-    interceptor.intercept(request, httpHandlerMock);
+    interceptor.intercept(httpRequestMock, httpHandlerMock);
 
-    expect(httpHandlerMock.handle).toHaveBeenCalledWith(request);
+    expect(httpHandlerMock.handle).toHaveBeenCalledWith(httpRequestMock);
   });
+
+  // it('should handle expired token and refresh it', async () => {
+  //   authServiceMock.isRefreshingToken = false;
+
+  //   const token = 'token';
+  //   authServiceMock.getToken.mockReturnValue(token);
+
+  //   mockJwtDecode.mockImplementation(() => ({
+  //     exp: Date.now() / 1000 - 60,
+  //   }));
+
+  //   const newToken = 'newToken';
+  //   authServiceMock.refreshToken.mockReturnValue(
+  //     of({
+  //       access_token: newToken,
+  //       user_info: {
+  //         id: '1',
+  //         email: 'test@example.com',
+  //         name: 'Test User',
+  //       },
+  //       refresh_token: 'refreshToken',
+  //     })
+  //   );
+
+  //   httpHandlerMock.handle.mockImplementation(() => of(new HttpResponse()));
+
+  //   await interceptor.intercept(httpRequestMock, httpHandlerMock).toPromise();
+
+  //   expect(httpRequestMock.clone).toHaveBeenCalledWith({
+  //     setHeaders: { Authorization: `Bearer ${newToken}` },
+  //   });
+  // });
 });
