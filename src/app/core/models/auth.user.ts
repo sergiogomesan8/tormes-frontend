7bfcc85d4f553bc53f02551af7f3188ddc577411
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-09 22:13:28 +0100

    update auth user models #30

--- src/app/core/models/auth.user.ts
+++ src/app/core/models/auth.user.ts
@@ -1,11 +1,15 @@
-import { User } from '@shop/models/user.model';
+import { User, UserType } from '@shop/models/user.model';
 
 export interface AuthUser {
   user_info: User;
-  token: string;
+  access_token: string;
+  refresh_token: string;
 }
 
 export interface TokenPayload {
+  id: string;
   email: string;
+  name: string;
+  userType: UserType;
   exp: number;
 }
