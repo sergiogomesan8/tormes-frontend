0f04a9d1927a0b7531613db3f80316868b610f39
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-11 00:26:13 +0100

    create interaction component #25

--- projects/tormes-library/src/lib/components/components.module.ts
+++ projects/tormes-library/src/lib/components/components.module.ts
@@ -5,10 +5,21 @@ import { MatIconModule } from '@angular/material/icon';
 import { SearchComponent } from './search/search.component';
 import { MatButtonModule } from '@angular/material/button';
 import { FileUploadComponent } from './file-upload/file-upload.component';
+import { InteractionControlComponent } from './interaction-control/interaction-control.component';
 
 @NgModule({
   imports: [CommonModule, MatButtonModule, MatIconModule],
-  declarations: [ButtonComponent, SearchComponent, FileUploadComponent],
-  exports: [ButtonComponent, SearchComponent, FileUploadComponent],
+  declarations: [
+    ButtonComponent,
+    SearchComponent,
+    FileUploadComponent,
+    InteractionControlComponent,
+  ],
+  exports: [
+    ButtonComponent,
+    SearchComponent,
+    FileUploadComponent,
+    InteractionControlComponent,
+  ],
 })
 export class ComponentsModule {}
