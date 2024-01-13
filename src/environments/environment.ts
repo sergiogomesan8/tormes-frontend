6e9a3ac0c751c7875204a8f07aae473033fd81b6
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-13 15:42:50 +0100

    little udpates #34

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
+// export const environment = {
+//   production: false,
+//   tormes_backend_api: 'http://localhost:3000/tormes/api',
+//   tormes_backend_images:
+//     'http://localhost:3000/tormes/images',
+// };
+
 export const environment = {
-  production: false,
-  tormes_backend_api: 'http://localhost:3000/tormes/api',
-  tormes_backend_images:
-    'http://localhost:3000/tormes/images',
+  production: true,
+  tormes_backend_api: 'https://tormes-backend-cba13a5fed1c.herokuapp.com:3000/tormes/api',
+  tormes_backend_images: 'https://tormes-backend-cba13a5fed1c.herokuapp.com:3000/tormes/images',
 };
