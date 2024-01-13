76fc79d309599f22d40aa781958f9ed4214e8735
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-13 21:23:44 +0100

    remove port from api call  #34

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  tormes_backend_api: 'https://tormes-backend-cba13a5fed1c.herokuapp.com:3000/tormes/api',
-  tormes_backend_images: 'https://tormes-backend-cba13a5fed1c.herokuapp.com:3000/tormes/images',
+  tormes_backend_api: 'https://tormes-backend-cba13a5fed1c.herokuapp.com/tormes/api',
+  tormes_backend_images: 'https://tormes-backend-cba13a5fed1c.herokuapp.com/tormes/images',
 };
