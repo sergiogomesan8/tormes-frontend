350062a0e6cd03f4b2fe93aceb26c7d80aa4d638
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-13 14:26:07 +0100

    remove prod environemnts #34

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-//   TORMES_BACKEND_API: 'http://localhost:3000/tormes/api',
-//   TORMES_BACKEND_IMAGES_PRODUCTS:
-//     'http://localhost:3000/tormes/images/products',
-//   TORMES_BACKEND_IMAGES_SECTIONS:
-//     'http://localhost:3000/tormes/images/products',
-};
