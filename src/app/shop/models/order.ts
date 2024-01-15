b47d16613903b659e0aabe12bda1a3775c2327f3
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-15 23:35:27 +0100

    create order model #24

---
+++ src/app/shop/models/order.ts
@@ -0,0 +1,35 @@
+import { Product } from './product';
+import { User } from './user.model';
+
+export interface Order {
+  id: string;
+  orderId: string;
+
+  status: OrderStatus;
+
+  date: number;
+  total: number;
+  trackingNumber?: string;
+
+  customer: User;
+  customerName?: string;
+  customerContact?: number;
+  deliveryAddress?: string;
+  billingAddress?: string;
+  paymentMethod?: string;
+
+  orderedProducts: OrderedProduct[];
+}
+
+export interface OrderedProduct {
+  productId: Product['id'];
+  amount: number;
+}
+
+export enum OrderStatus {
+  processing = 'processing',
+  shipped = 'shipped',
+  delayed = 'delayed',
+  delivered = 'delivered',
+  cancelled = 'cancelled',
+}
