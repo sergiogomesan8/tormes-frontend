cf39d2f6c39fd9f269bcd2c0432beab530751ed0
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-19 22:31:01 +0100

    update environments #36

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,5 @@
-// export const environment = {
-//   production: false,
-//   tormes_backend_api: 'http://localhost:3000/tormes/api',
-//   tormes_backend_images:
-//     'http://localhost:3000/tormes/images',
-// };
-
 export const environment = {
-  production: true,
-  tormes_backend_api: 'https://tormes-backend-cba13a5fed1c.herokuapp.com/tormes/api',
-  tormes_backend_images: 'https://tormes-backend-cba13a5fed1c.herokuapp.com/tormes/images',
+  production: false,
+  tormes_backend_api: 'http://localhost:3000/tormes/api',
+  tormes_backend_images: 'http://localhost:3000/tormes/images',
 };
