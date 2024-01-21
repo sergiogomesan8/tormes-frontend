ae30950ad580c802c27be7aa2a979c08dc04a52c
SergioGómez <sergio.gomesan8@gmail.com>
2024-01-21 16:42:46 +0100

    create cash register dtos #43

---
+++ src/app/shop/admin/dtos/cash-register.dto.ts
@@ -0,0 +1,51 @@
+import { Bills, Coins } from '@shop/models/cash-register';
+
+export class CreateCashRegisterDto {
+  coins: Coins;
+  bills: Bills;
+  totalCardPayments: number;
+  totalSpent: number;
+  cashInBox: number;
+  reportedTotal: number;
+
+  constructor(
+    coins: Coins,
+    bills: Bills,
+    totalCardPayments: number,
+    totalSpent: number,
+    cashInBox: number,
+    reportedTotal: number
+  ) {
+    this.coins = coins;
+    this.bills = bills;
+    this.totalCardPayments = totalCardPayments;
+    this.totalSpent = totalSpent;
+    this.cashInBox = cashInBox;
+    this.reportedTotal = reportedTotal;
+  }
+}
+
+export class UpdateCashRegisterDto {
+  coins?: Coins;
+  bills?: Bills;
+  totalCardPayments?: number;
+  totalSpent?: number;
+  cashInBox?: number;
+  reportedTotal?: number;
+
+  constructor(
+    coins?: Coins,
+    bills?: Bills,
+    totalCardPayments?: number,
+    totalSpent?: number,
+    cashInBox?: number,
+    reportedTotal?: number
+  ) {
+    this.coins = coins;
+    this.bills = bills;
+    this.totalCardPayments = totalCardPayments;
+    this.totalSpent = totalSpent;
+    this.cashInBox = cashInBox;
+    this.reportedTotal = reportedTotal;
+  }
+}
